dfbb3e21d11803a70642531b5a7ab3f9c15c76bd
AnnieChen <annie8528@gmail.com>
2020-04-27 10:17:29 +0800

    a001

---
+++ zerojudge/a001.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 05 19:55:36 2020
+
+@author: fdbfvuie
+"""
+
+a = input()
+print("hello,", a)
---
+++ zerojudge/a002..py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 05 19:59:36 2020
+
+@author: fdbfvuie
+"""
+
+num = input()
+numlist = num.split(" ")
+print(int(numlist[0]) + int(numlist[1]))
