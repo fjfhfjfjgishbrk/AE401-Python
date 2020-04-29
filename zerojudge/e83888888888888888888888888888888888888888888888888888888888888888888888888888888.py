50b85ce9b94da016862b361140987892854966e1
fjfhfjfjgishbrk <59891511+fjfhfjfjgishbrk@users.noreply.github.com>
2020-04-29 10:24:13 +0800

    推

---
+++ zerojudge/e83888888888888888888888888888888888888888888888888888888888888888888888888888888.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 10:13:39 2020
+
+@author: fdbfvuie
+"""
+
+
+n = int(input())
+bomb = [[],[]]
+for i in range(n):
+    a = input()
+    for j in range(len(a)):
+        if a[j] == "*":
+            bomb[0].append(j)
+            bomb[1].append(i)
+for i in range(n):
+    for j in range(n):
+        if j in bomb[0] or i in bomb[1]:
+            print("*", end="")
+        else:
+            print("0", end="")
+    print()
