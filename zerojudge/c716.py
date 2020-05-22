da409042e2d11c56a30969a3be13f97f138b3022
AnnieChen <annie8528@gmail.com>
2020-05-22 10:40:15 +0800

    d051

---
+++ zerojudge/c716.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 22 10:04 2020
+
+@author: fdbfvuie
+"""
+
+print("Go, {}, go go".format(input()))
---
+++ zerojudge/c726.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 22 10:07 2020
+
+@author: fdbfvuie
+"""
+
+a, b = map(str, input().split())
+print("{} and {} sitting in the tree".format(a, b))
