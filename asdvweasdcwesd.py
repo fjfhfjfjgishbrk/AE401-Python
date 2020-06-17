a33f555a1a166ed03cfaab2606204796d8efcf6f
fjhrgjkw <hello931118@gmail.com>
2020-06-17 18:46:42 +0800

    orangeUpload
    orangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorangeorange

--- asdvweasdcwesd.py
+++ asdvweasdcwesd.py
@@ -313,3 +313,189 @@ while True:
         break
 """
 
+
+"""
+a = input().split()
+b = input().split()
+c = [int(i) for i in input().split()]
+d = 0
+e = True
+for i in range(len(a)):
+    for j in range(len(b)):
+        if a[i] == b[j]:
+            if i == 2:
+                d = max(0, d - c[j])
+                e = False
+            else:
+                d += c[j]
+if e:
+    d *= 2
+print(d)
+"""
+
+"""
+n = int(input())
+for i in range(n):
+    a = int(input())
+    b = [0] * 10
+    for j in range(1, a + 1):
+        for k in str(j):
+            b[int(k)] += 1
+    print(" ".join([str(j) for j in b]))
+"""
+
+"""
+import math
+d = {'gb': 8000000000,  "mb": 8000000, "kb": 8000, "byte": 8, "bit": 1, "g": 8000000000, "m": 8000000, "k": 8000}
+while True:
+    try:
+        a = input()
+        b = 0
+        c = []
+        for i in range(len(a)-1):
+            if a[i].isalpha() != a[i+1].isalpha():
+                c.append(a[b:i+1])
+                b = i+1
+        c.append(a[b:])
+        t = 0
+        for i in range(0, len(c), 2):
+            c[i] = math.modf(float(c[i]))[0] * 1.25 + math.modf(float(c[i]))[1] if c[i+1] != "kb" else float(c[i])
+            t += int(c[i] * int(d[c[i+1]]))
+        print(t)
+    except:
+        break
+"""
+
+"""
+while True:
+    a = input().lower().strip()
+    b = 0
+    c = False
+    if a == "0":
+        break
+    for i in a:
+        if i.isalpha():
+            b += ord(i) - 96
+        else:
+            c = True
+            break
+    if c:
+        print("Fail")
+    else:
+        print(b)
+"""
+
+"""
+n = int(input())
+a = {}
+for i in range(n):
+    b, c = map(str, input().split())
+    a[b] = float(c)
+d = {i: j for i, j in sorted(a.items(), key=lambda item: item[1])}
+e = []
+for i in range(int(n/8)):
+    e.append([])
+f = [i for i, j in d.items()]
+g = 0
+countUp = True
+for i in range(n):
+    e[g].append(f[i])
+    g = g + 1 if countUp else g - 1
+    if g == int(n/8):
+        g -= 1
+        countUp = False
+    elif g == -1:
+        g += 1
+        countUp = True
+h = []
+for i in e:
+    h.append([])
+    for j in "75312468":
+        h[-1].append(i[int(j) - 1])
+for i in range(len(h)):
+    print("{} {}".format(str(i+1), " ".join(h[i])))
+"""
+
+
+"""
+while 1:
+    try:
+        a = input().split("/")
+        a = int(a[0]) * 100 + int(a[1])
+        if a < 121:
+            print("摩羯座")
+        elif a < 220:
+            print("水瓶座")
+        elif a < 321:
+            print("雙魚座")
+        elif a < 421:
+            print("牡羊座")
+        elif a < 522:
+            print("金牛座")
+        elif a < 622:
+            print("雙子座")
+        elif a < 723:
+            print("巨蟹座")
+        elif a < 822:
+            print("獅子座")
+        elif a < 924:
+            print("處女座")
+        elif a < 1024:
+            print("天秤座")
+        elif a < 1123:
+            print("天蠍座")
+        elif a < 1223:
+            print("射手座")
+        else:
+            print("摩羯座")
+    except:
+        break
+"""
+
+"""
+n = int(input())
+pos = 0
+c = 0
+for i in range(n):
+    a = input()
+    if a == "L":
+        pos -= 1
+    elif a == "R":
+        pos += 1
+    else:
+        b = int(a.split()[1])
+        if pos == b:
+            c += 1
+print(c)
+"""
+
+"""
+n = int(input())
+input()
+for i in range(n):
+    a = int(input())
+    b = int(input())
+    for j in range(b):
+        for k in range(1, a+1):
+            print(str(k)*k)
+        for k in range(a-1, 0, -1):
+            print(str(k)*k)
+        print()
+"""
+
+
+
+import math
+while 1:
+    try:
+        a = round(float(input()) * 100)
+        if a <= 10000:
+            a = a * 0.9 + 800
+        elif a <= 50000:
+            a = a * 0.8
+        else:
+            a = a * 0.6
+        a = math.floor(a)/100
+        print("$" + "{:.2f}".format(a))
+    except:
+        break
